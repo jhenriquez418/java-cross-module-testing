0410828ff1b5a54ef14b19d52f8eef6ab7883310
jhenriquez418 <jhenri02@yahoo.com>
2018-12-05 23:24:06 -0500

    Initializing project with source code

---
+++ Declaration/src/main/java/com/example/declaration/Foo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.declaration;
+
+
+/**
+ *
+ * @author jhenriquez
+ */
+public interface Foo
+{
+    int addIntegers(int one, int two);
+    
+}
